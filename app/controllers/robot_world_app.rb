8f6aaeaa85a87a07a9700f4f889a7f522fe59754
Michael Dorrance <dorrancemd@gmail.com>
2015-07-08 19:07:13 -0600

    add mdl framework

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,12 +1,52 @@
 require 'models/robot_world'
+require 'date'
 
 class RobotWorldApp < Sinatra::Base
 	set :root, File.join(File.dirname(__FILE__), '..')
 	set :method_override, true
 
 	get '/' do
-		'hello, world'
+		@average_robot_age = RobotWorld.average_robot_age
+		@robots_hired_per_year = RobotWorld.robots_hired_per_year
+		@robots_per_department = RobotWorld.robots_per_department
+		@robots_per_city = RobotWorld.robots_per_city
+		@robots_per_state = RobotWorld.robots_per_state
 		erb :dashboard
 	end
-	
+
+	get '/robots' do
+		@robots = RobotWorld.all
+		erb :index
+	end
+
+	get '/robots/new' do
+		erb :new
+	end
+
+	post '/robots' do
+		RobotWorld.create(params[:robot])
+		redirect '/robots'
+	end
+
+	get '/robots/:id' do |id|
+		@robot = RobotWorld.find(id.to_i)
+		erb :show
+	end
+
+	get '/robots/:id/edit' do |id|
+		@robot = RobotWorld.find(id.to_i)
+		erb :edit
+	end
+
+	put '/robots/:id' do |id|
+		RobotWorld.update(id.to_i, params[:robot])
+		redirect "/robots/#{id}"
+	end
+
+	delete '/robots/:id' do |id|
+		RobotWorld.delete(id.to_i)
+		redirect "/robots"
+	end
+
+
 end
---
+++ app/models/robot_world.rb
@@ -0,0 +1,124 @@
+require 'yaml/store'
+require 'date'
+require 'bigdecimal'
+
+require_relative 'robot'
+
+class RobotWorld
+	def self.database
+		@database ||= YAML::Store.new("db/robot_world")
+	end
+
+	def self.create(robot)
+		database.transaction do
+			database['robots'] ||= []
+			database['total'] ||= 0
+			database['total'] += 1
+			database['robots'] << { "id" => database['total'], 
+															"name" => robot[:name], 
+															"city" => robot[:city], 
+															"state" => robot[:state], 
+															"avatar" => robot[:avatar],
+															"birthdate" => robot[:birthdate],
+															"date_hired" => robot[:date_hired],
+															"department" => robot[:department] 
+														}
+		end
+	end
+
+	def self.raw_robots
+		database.transaction do
+			database['robots'] || []
+		end
+	end
+
+	def self.all
+		raw_robots.map { |data| Robot.new(data) }
+	end
+
+	def self.raw_robot(id)
+		raw_robots.find { |robot| robot["id"] == id }
+	end
+
+	def self.find(id)
+		Robot.new(raw_robot(id))
+	end
+
+	def self.update(id, robot)
+		database.transaction do
+			target = database["robots"].find { |data| data["id"] == id }
+			target["name"] = robot[:name]
+			target["city"] = robot[:city]
+			target["state"] = robot[:state]
+			target["avatar"] = robot[:avatar]
+			target["birthdate"] = robot[:birthdate]
+			target["date_hired"] = robot[:date_hired]
+			target["department"] = robot[:department]
+		end
+	end
+
+	def self.delete(id)
+		database.transaction do
+				database["robots"].delete_if { |robot| robot["id"] == id  }
+		end
+	end
+
+  def self.average_robot_age
+    count = 0
+    database.transaction do
+      ages = database['robots'].map do |robot|
+        count += 1
+        ((Date.today - Date.strptime(robot["birthdate"],'%m/%d/%Y')).to_i) / 365.0
+      end
+      (ages.reduce(:+) / count).round(2)
+    end
+  end
+
+  def self.robots_hired_per_year
+    hired = Hash.new { |hsh, key| hsh[key] = 0}
+    database.transaction do
+        database['robots'].map do |robot|
+          year = Date.strptime(robot["date_hired"],'%m/%d/%Y').year
+          hired[year] += 1
+        end
+      hired
+      end
+  end
+
+  def self.robots_per_department
+    per_department = Hash.new { |hsh, key| hsh[key] = 0}
+    database.transaction do
+        database['robots'].map do |robot|
+          department = robot["department"]
+          per_department[department] += 1
+        end
+      per_department
+      end
+  end
+
+  def self.robots_per_city
+    per_city = Hash.new { |hsh, key| hsh[key] = 0}
+    database.transaction do
+        database['robots'].map do |robot|
+          city = robot["city"]
+          per_city[city] += 1
+        end
+      per_city
+      end
+  end
+
+  def self.robots_per_state
+    per_state = Hash.new { |hsh, key| hsh[key] = 0}
+    database.transaction do
+        database['robots'].map do |robot|
+          state = robot["state"]
+          per_state[state] += 1
+        end
+      per_state
+      end
+  end
+	
+
+
+
+end
