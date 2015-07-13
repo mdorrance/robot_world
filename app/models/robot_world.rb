0239ade46f825b11c5f8f2737fec9724b972ab92
Michael Dorrance <dorrancemd@gmail.com>
2015-07-13 08:39:47 -0600

    deletes unused methods and commented out code in robot_world.rb

--- app/models/robot_world.rb
+++ app/models/robot_world.rb
@@ -14,22 +14,6 @@ class RobotWorld
 	end
 
 	def self.create(robot)
-		# database.transaction do
-		# 	database['robots'] ||= []
-		# 	database['total'] ||= 0
-		# 	database['total'] += 1
-		# 	database['robots'] << { "id" => database['total'], 
-		# 													"name" => robot[:name], 
-		# 													"city" => robot[:city], 
-		# 													"state" => robot[:state], 
-		# 													"avatar" => robot[:avatar],
-		# 													"birthdate" => robot[:birthdate],
-		# 													"date_hired" => robot[:date_hired],
-		# 													"department" => robot[:department] 
-		# 												}
-
-
-		
 		begin
 			dataset.insert( name: robot[:name],
 											city: robot[:city],
@@ -44,18 +28,8 @@ class RobotWorld
 		Robot.new(dataset.to_a.last)
 	end
 
-	def self.raw_robots
-		database.transaction do
-			database['robots'] || []
-		end
-	end
-
 	def self.all
 		dataset.map { |data| Robot.new(data) }
-	end
-
-	def self.raw_robot(id)
-		raw_robots.find { |robot| robot["id"] == id }
 	end
 
 	def self.find(id)
@@ -63,24 +37,11 @@ class RobotWorld
 	end
 
 	def self.update(id, robot)
-		# database.transaction do
-		# 	target = database["robots"].find { |data| data["id"] == id }
-		# 	target["name"] = robot[:name]
-		# 	target["city"] = robot[:city]
-		# 	target["state"] = robot[:state]
-		# 	target["avatar"] = robot[:avatar]
-		# 	target["birthdate"] = robot[:birthdate]
-		# 	target["date_hired"] = robot[:date_hired]
-		# 	target["department"] = robot[:department]
-		# end
 		target = dataset.where(id: id)
 		dataset.where(id: id).update(robot)
 	end
 
 	def self.delete(id)
-		# database.transaction do
-		# 		database["robots"].delete_if { |robot| robot["id"] == id  }
-		# end
 		dataset.where(id: id).delete
 	end
 
@@ -144,17 +105,11 @@ class RobotWorld
   end
 	
 	 def self.delete_all
-    # database.transaction do
-    #   database['robots'] = []
-    #   database['total'] = 0
-    # end
     dataset.delete
   end
 
   def self.dataset
   	database.from(:robots)
   end
-
-
 
 end
