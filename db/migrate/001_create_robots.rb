f7e11f7291e0db11a2c9e3e6ea122306e2cb9b1b
Michael Dorrance <dorrancemd@gmail.com>
2015-07-12 21:42:23 -0600

    creates migration file

---
+++ db/migrate/001_create_robots.rb
@@ -0,0 +1,18 @@
+require "sequel"
+require "sqlite3"
+require "./app/models/robot_world"
+
+environments = %w(test development)
+
+environments.each do |environment|
+	Sequel.sqlite("db/robot_world_#{environment}.sqlite3").create_table :robots do
+		primary_key :id
+		String			:name
+		String  		:city
+		String  		:avatar
+		String  		:birthdate
+		String  		:date_hired
+		String  		:department
+	end
+	puts "creating robtos table for #{environment}"
+end
